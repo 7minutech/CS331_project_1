50010ff84cf2b860669d9400e8a492f43328ada1
7minutech <elijohnson1107@gmail.com>
2024-09-05 13:55:35 -0500

    refactor: changed access and added getter for move

--- lib/computer.py
+++ lib/computer.py
@@ -3,12 +3,14 @@ class Computer:
     # style: capitalized MOVE_SET to show it's a constant
     MOVE_SET = ["rock","paper", "scissors"]
     def __init__(self):
-        self.move  = ""
+        self.__move  = ""
         self.wins = 0
         self.losses = 0
         self.ties = 0
     def set_move(self):
-        self.move = rand.choice(Computer.MOVE_SET)
+        self.__move = rand.choice(Computer.MOVE_SET)
+    def get_move(self):
+        return self.__move
     
 
     
